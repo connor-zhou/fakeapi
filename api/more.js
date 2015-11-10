4c6fa77bbbc1fc51e211575aa19103968a996f77
xwy <xiewuyuan@fdjf.net>
2015-11-10 09:41:56 +0800

    001

--- api/more.js
+++ api/more.js
@@ -59,7 +59,8 @@ router.all('/more/loan', function (req, res, next) {
  *  	imageUrl:"{string} 	图片URL（绝对路径）",
  *  	title:"{string} 标题",
  *  	type:"{int} 类型（1--活动，点击后打开url；2--项目，点击后跳到项目详情",
- *  	target:"{string} 目标参数"
+ *  	target:"{string} 目标参数",
+ *  	activity_period:"{string} 活动周期"
  *     }
  * 	]
  * }
@@ -73,7 +74,8 @@ router.all('/more/activityPageList', function (req, res, next) {
     		imageUrl:'https://www.hsbank360.com/static/modules/front/images/index/banner-0' + i + '.jpg', 
         	title: 'slide'+i, 
         	type: [1,2][i % 2],
-        	target: [1,2][i % 2] == 1 ? "https://www.hsbank360.com/f/activity/invitation" : "1"
+        	target: [1,2][i % 2] == 1 ? "https://www.hsbank360.com/f/activity/invitation" : "1",
+        	activity_period:'2015-11-09至2015-12-09'
         });
     });
     var resultValue = {
