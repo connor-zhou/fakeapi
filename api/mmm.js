7b7ee321f005c561a6d1eba1535181f325082cf7
leiliang <leiliangliangok@163.com>
2016-04-18 17:24:04 +0800

    mmm接口 添加调用成功后的callbackUrl

--- api/mmm.js
+++ api/mmm.js
@@ -11,6 +11,7 @@ var router = express.Router();
  * @input.post {String} token 			Token
  * @input.post {String} realName 		真实姓名
  * @input.post {String} chinaId 		身份证号码
+ * @input.post {String} callbackUrl   注册成功后的跳转地址（只h5页面对应接口需要传入）
  *
  * @description
  *
@@ -113,9 +114,10 @@ router.all('/mmm/toUnBindBankCard', function (req, res, next) {
  * @name mmm.toRecharge
  * @href /mmm/toRecharge
  *
- * @input.post {String} client 		客户端统计参数（common/client）
- * @input.post {String} token 			Token
- * @input.post {String} money 		充值金额
+ * @input.post {String} client 		 客户端统计参数（common/client）
+ * @input.post {String} token 			 Token
+ * @input.post {String} money 		     充值金额
+ * @input.post {String} callbackUrl    充值成功后的跳转地址 （只h5页面对应接口需要传入）
  *
  * @description
  *
@@ -152,6 +154,7 @@ router.all('/mmm/toRecharge', function (req, res, next) {
  * @input.post {String} token 			 Token
  * @input.post {String} money 		     提现金额
  * @input.post {String} cardNo         提现银行卡号
+ * @input.post {String} callbackUrl    提现成功后的跳转地址 （只h5页面对应接口需要传入）
  *
  * @description
  *
@@ -190,6 +193,7 @@ router.all('/mmm/toWithdraw', function (req, res, next) {
  * @input.post {String}   projectId				项目Id
  * @input.post {String}   money					投资金额
  * @input.post {String}   awardIds				    投资券Ids（多个id用 ","(英文逗号)拼接）
+ * @input.post {String}   callbackUrl             投资成功后的跳转地址 （只h5页面对应接口需要传入）
  * 
  * @output {json} 投资的URL
  * {
