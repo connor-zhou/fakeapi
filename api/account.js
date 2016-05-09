6b7e1ed914fa09fff0ebcae120adb6b531b2a695
leiliang <leiliangliangok@163.com>
2016-05-09 09:20:45 +0800

    修改我的邀请接口注释

--- api/account.js
+++ api/account.js
@@ -960,8 +960,8 @@ router.all('/account/sign', function (req, res, next) {
  * @input.post {string} client 		 客户端统计参数（common/client）
  * @input.post {string} token			 Token
  * @input.post {string} status			 状态（如果为空，则取全部）
- * @input.post {int=} [pageSize=10] 	 页容量
- * @input.post {int=} [pageNumber=1] 	 页码
+ * @input.post {int=} [pageSize=10] 	 好友列表页容量
+ * @input.post {int=} [pageNumber=1] 	 好友列表页码
  *
  * @output {json} 分页列表
  * {
