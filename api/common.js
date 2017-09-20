6dae7e29c0bd6de907a56e972d7167aafd8db34d
leiliang <leiliangliang@xingtouzi.com>
2017-09-20 19:37:46 +0800

    添加图片

--- api/common.js
+++ api/common.js
@@ -323,7 +323,7 @@ var _ = require('lodash');
  *
  * https://192.168.1.86:3000/common/imageCode?client="LKJLSJFDAL"&uniqueId="SAFSFS"
  *
- * @output {string} 返回图片数据data
+ * @output {string} 返回图片数据data（base64编码）
  *
  * */
 
@@ -336,7 +336,7 @@ router.all('/common/imageCode',function(req,res,next){
         }
     };
 
-    res.json('');
+    res.json('data:image/png;base64,　iVBORw0KGgoAAAANSUhEUgAAAAEAAAAkCAYAAABIdFAMAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAHhJREFUeNo8zjsOxCAMBFB/　KEAUFFR0Cbng3nQPw68ArZdAlOZppPFIBhH5EAB8b+Tlt9MYQ6i1BuqFaq1CKSVcxZ2Acs6406KUgpt5/　LCKuVgz5BDCSb13ZO99ZOdcZGvt4mJjzMVKqcha68iIePB86GAiOv8CDADlIUQBs7MD3wAAAABJRU5ErkJggg==');
 });
 
 /**
